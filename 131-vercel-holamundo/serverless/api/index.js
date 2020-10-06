ee0757a5cd086f9345248485022626bea77d0191
Renier Ferrer <rferrer@isc-bunkerramo.com>
2020-10-05 22:08:14 -0400

    [MOD] Configuracion: Variables de entorno y librerias necesarias.

---
+++ 131-vercel-holamundo/serverless/api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Invocamos metodos necesarios para el servicio
+const app = express()
+app.use(bodyParser.json())
+/*app.use(cors())*/
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+
+// Iniciamos el servicio.
+app.get('*', (request, response) => {
+    response.send('Chanchito Feliz')
+})
+
+module.exports = app
