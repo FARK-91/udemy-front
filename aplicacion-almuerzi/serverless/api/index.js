d8f5878367b1aec6912cbc07eeb9c66f51ac0fc8
Renier Ferrer <rferrer@isc-bunkerramo.com>
2020-10-23 20:04:41 -0400

    [MOD]

--- aplicacion-almuerzi/serverless/api/index.js
+++ aplicacion-almuerzi/serverless/api/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // Invocamos metodos necesarios para el servicio
 const app = express()
 app.use(bodyParser.json())
-/*app.use(cors())*/
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 
