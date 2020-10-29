11d150a8df218fba56104b9a5c7f7b2097b6854d
Renier Ferrer <rferrer@isc-bunkerramo.com>
2020-10-29 07:48:25 -0400

    [ADD] Nuevo avance

--- aplicacion-almuerzi/serverless/api/index.js
+++ aplicacion-almuerzi/serverless/api/index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
+
 // Invocamos metodos necesarios para el servicio
 const app = express()
 app.use(bodyParser.json())
@@ -10,9 +13,8 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 
-// Iniciamos el servicio.
-app.get('*', (request, response) => {
-    response.send('Chanchito Feliz')
-})
+// Llamamos las rutas necesarias.
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
 
 module.exports = app
